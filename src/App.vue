54044b5875bf72ebaf90abe5b70c106e8d082d9c
Stefanie Cruz <crzstefanie@gmail.com>
2022-04-24 14:48:56 +0100

    Add vue files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>CRUD App</strong></router-link>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+            <div class="navbar-item">
+              <router-link to="/" class="navbar-item">Home</router-link>
+              <div class="buttons">
+                <router-link to="/sign-up" class="button is-success"><strong>Link 1</strong></router-link>
+              </div>
+            </div>
+
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1 class="title">CRUD App built with Disease.sh API</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
